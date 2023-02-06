35380b0a200a21586ca7463a652f579bfd89a221
Jacob Snarr <jacobsnarr@gmail.com>
2023-02-05 23:15:48 -0500

    Tweak layouts and upgrade gradle version

--- app/src/main/java/edu/temple/convoy/MainActivity.kt
+++ app/src/main/java/edu/temple/convoy/MainActivity.kt
@@ -1,11 +1,19 @@
 package edu.temple.convoy
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
+        val signUpButton = findViewById<Button>(R.id.signUpButton)
+        signUpButton.setOnClickListener {
+//            val intent = Intent(this, SignUpActivity::class.java)
+//            startActivity(intent)
+        }
     }
 }
